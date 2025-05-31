7a039d4c4c097863d0ccde68fe5da5700883df18
Ph_Chags <ph_chagas@id.uf.br>
2025-05-31 13:54:25 -0300

    fix gh pages problem. ((finalmente funcionando)

--- src/App/App.tsx
+++
@@ -1,9 +0,0 @@
-// App.tsx
-import { RouterProvider } from 'react-router-dom';
-import router from '../routes/router.tsx';
-
-function App() {
-  return <RouterProvider router={router} />;
-}
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App/App'; 
+import { RouterProvider } from 'react-router-dom';
+import router from './routes/router';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import './index.css';
+import './index.css'; 
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <App /> 
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
