098ffc8441d53a0ab5e1a3ad5c1ab907f1085292
Ph_Chags <ph_chagas@id.uf.br>
2025-05-31 13:44:28 -0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App'; 
+import App from './App/App'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './index.css';
